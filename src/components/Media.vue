10315ed04f60727e9001c5fa84e0e567a81518d5
Tyler Place <tylerlplace@gmail.com>
2023-03-07 22:34:33 -0800

    Removed Folder View & Updated Media View
    - Added custom controls for video player
    - Added selected emit for media
    - Added options to have media thumbnail only and autoplay

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -2,20 +2,40 @@
 import { fileTypes, type FileData } from '@/types';
 import { ref } from 'vue';
 
-defineProps<{
+const emit = defineEmits(['selected']);
+
+const props = defineProps<{
     file: FileData,
+    thumbOnly?: boolean,
+    autoplay?: boolean,
     maxW?: string,
     maxH?: string,
 }>()
 
-var videoLoaded = ref(false);
+// Video stuff
+
+var videoEl = ref<HTMLVideoElement|null>(null);
+var videoLoaded = ref(!!props.autoplay);
+var paused = ref(false);
+
+function isSmallScreen() {
+    return window.innerWidth < 768;
+}
+
+function toggleVideo() {
+    if (videoEl.value==null) return;
+    paused.value = !videoEl.value.paused;
+    if (videoEl.value.paused) videoEl.value.play();
+    else videoEl.value.pause();
+}
 
 </script>
 
 <template>
-    <img v-if="fileTypes[file.type].startsWith('image')" :src="file.src" loading="lazy" class="media" :style="{maxWidth: maxW, maxHeight: maxH}">
+    <img v-if="fileTypes[file.type].startsWith('image')" :src="file.src" loading="lazy" class="media" :style="{maxWidth: maxW, maxHeight: maxH}" @click="emit('selected')">
     
-    <div v-if="(fileTypes[file.type].startsWith('video') && !videoLoaded)" class="relative" @click="videoLoaded = true">
+    <div v-if="(fileTypes[file.type].startsWith('video') && !videoLoaded)" class="relative" 
+        @click="if (!thumbOnly) videoLoaded = true; if (isSmallScreen()) $emit('selected');">
         <img :src="/*file.src.replace(/\.[a-zA-Z0-9]+$/, '.jpg')*/'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'" loading="lazy" class="media" :style="{maxWidth: maxW, maxHeight: maxH}">
         <div class="absolute flex justify-center items-center top-0 left-0 bottom-0 right-0">
             <div class="p-2 bg-black bg-opacity-50 rounded-[50%] hover:bg-opacity-60 cursor-pointer">
@@ -23,9 +43,17 @@ var videoLoaded = ref(false);
             </div>
         </div>
     </div>
-    <video v-if="(fileTypes[file.type].startsWith('video') && videoLoaded)" controls class="media" :style="{maxWidth: maxW, maxHeight: maxH}" autoplay>
-        <source :src="file.src" :type="fileTypes[file.type]">
-    </video>
+    <div v-if="(fileTypes[file.type].startsWith('video') && videoLoaded)" class="relative cursor-pointer group" @click="toggleVideo"  @dblclick="emit('selected')">
+        <video autoplay ref="videoEl" class="media" :style="{maxWidth: maxW, maxHeight: maxH}">
+            <source :src="file.src" :type="fileTypes[file.type]">
+        </video>
+        <div class="absolute hidden group-hover:flex justify-center items-center top-0 left-0 bottom-0 right-0">
+            <div class="p-2 bg-black bg-opacity-50 rounded-[50%] hover:bg-opacity-60 cursor-pointer">
+                <svg v-if="paused" class="w-8 h-8" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M320 853V293l440 280-440 280Z"/></svg>
+                <svg v-if="!paused" class="p-2 w-8 h-8" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 28"><path d="m16.25,28V0h7.75v28h-7.75Zm-16.25,0V0h7.75v28H0Z"/></svg>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,8 @@ import { createRouter, createWebHistory, type RouteLocation } from 'vue-router'
 import DashboardView from './views/home/DashboardView.vue'
 import LoginView from '@/views/LoginView.vue'
 import HomeView from '@/views/HomeView.vue'
-import FolderView from '@/views/home/dashboard/FolderView.vue';
 import UploadView from './views/home/UploadView.vue';
 import EditView from './views/home/EditView.vue';
-import MediaView from './views/home/dashboard/MediaView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,17 +21,7 @@ const router = createRouter({
             children: [
                 {
                     path: '',
-                    component: DashboardView,
-                    children: [
-                        {
-                           path: '',
-                            component: FolderView 
-                        },
-                        {
-                            path: 'media',
-                            component: MediaView
-                        }
-                    ]
+                    component: DashboardView
                 },
                 {
                     path: 'upload',
